f38c072dec934dc9d2758a18de8094c5e554da32
Guy Frydenlund <guyfrydenlund@Josephs-MacBook-Pro.local>
2021-01-27 21:07:15 -0700

    yes

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    .body
+      h1 Grim Fossa 2
+</template>
+
+<script>
+export default {
+    name: 'Home',
+};
+</script>
+
+<style lang="scss">
+.body {
+    background-color: hsla(117, 26%, 47%, .3);
+    height: 800px;
+};
+</style>
